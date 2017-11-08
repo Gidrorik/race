a826db4aa5853860341999b843e7e1d26a892973
Gidrorik <33127800+Gidrorik@users.noreply.github.com>
2017-11-08 23:44:35 +0200

    Update obstacle.h

--- race/obstacle.h
+++ race/obstacle.h
@@ -1,1 +1,20 @@
-#pragma once
+#define OBSTACLE_H
+
+#include "car.h"
+#include "Map.h"
+
+class Obstacle : public race
+{
+public:
+	Obstacle(race pos);
+
+	virtual void calcLeft() {};
+	virtual void calcRight() {};
+
+	inline race getPosition() const { return m_obstaclePosition; };
+
+private:
+	GameObjPosition m_obstaclePosition;
+};
+#endif OBSTACLE_Hce
+
